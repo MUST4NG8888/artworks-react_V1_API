896d1b42c28529ebdf9f99a115ed50793c654d6c
Drew <andrew.lendvai@gmail.com>
2022-11-29 18:22:37 +0100

    Hardcoded data

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,38 +1,40 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
-import Masonry from "@mui/lab/Masonry";
-import Box from '@mui/material/Box';
-import styles from "./Gallery.module.css"
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+
+import styles from "./Gallery.module.css";
 
 const Label = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(0.5),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
   borderBottomLeftRadius: 0,
   borderBottomRightRadius: 0,
 }));
 
-
 function Gallery({ data }) {
   return (
     <div id={styles.wrapper}>
       {console.log(data)}
-      <Masonry columns={3} spacing={2} >
-        {data.records.map((item, index) => (
-          <div>
-            <Label>{index + 1}</Label>
-            <img key={index} src={item.primaryimageurl} style={{
-                borderBottomLeftRadius: 4,
-                borderBottomRightRadius: 4,
-                display: 'block',
-                width: '100%',  }}></img>
-          </div>
+      <ImageList variant="masonry" cols={4} gap={25}>
+        {data.map((item, index) => (
+          <ImageListItem>
+            <img
+              src={`${item.image}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.title}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar position="below" title={item.title} />
+          </ImageListItem>
         ))}
-      </Masonry>
-      </div>
+      </ImageList>
+    </div>
   );
 }
 
--- src/utility/getData.js
+++ src/utility/getData.js
@@ -1,10 +1,14 @@
 const getData = async () => {
   let response = await fetch(
-    `https://api.harvardartmuseums.org/object?person="Monet"&size=50&apikey=57696d00-a401-4113-a94b-282c2baaa940`
+    `https://www.wikiart.org/en/api/2/PaintingSearch?term=Picasso&authSessionKey=a6accbaecccc`, 
+    {
+      mode: "cors"
+    }
   );
   let result = await response.json();
-  return result;
-    
+  
+  console.log(result);
+  return result;  
 };
 
 export default getData;
