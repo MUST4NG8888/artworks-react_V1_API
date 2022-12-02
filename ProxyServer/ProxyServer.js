018164b879909710147fae9536fa15e9e3ef4e3e
Drew <andrew.lendvai@gmail.com>
2022-12-02 09:02:46 +0100

    Artist 1560  push

--- ProxyServer/ProxyServer.js
+++ ProxyServer/ProxyServer.js
@@ -25,27 +25,70 @@ app.get("/", (req, res) => {
 });
 
 app.get("/artists", (req, res) => {
-  
-  const getArtists = async (token) => {
-    let artistResponse = null;
-    if (token !== undefined) {
-      artistResponse = await fetch(
-        `https://www.wikiart.org/en/api/2/UpdatedArtists?paginationToken=${token}`
-      );
-    } else {
-      artistResponse = await fetch(
-        `https://www.wikiart.org/en/api/2/UpdatedArtists`
-      );
-    }
 
-    let artistResult = await artistResponse.json();
-
+  const tokens =[
+    '',
+    '?paginationToken=iHUa6WAPF%2fzgSNu%2fzkfPRaS0MDRZMQk922gJj%2bOD2Jo%3d',
+    '?paginationToken=c1U2tRfbCX6%2bw8uWtQLMBdvMFC2T7DhVnt9wZNMeN6c%3d',
+    '?paginationToken=2kjyBuvG0YA8nozqk2%2fv%2fEjg2BzHbLG9FoCJEwejQvo%3d',
+    '?paginationToken=XPfYaMIaDTulUxxv1RknC6SpZbMuKEomEyeaQVPBv50%3d',
+    '?paginationToken=0qCIFw7RCBEpFCfIoHpY9ETClLwgeth4cttK3yHU2bU%3d',
+    '?paginationToken=X5QBk5VgByogGiEVCwt7KW8quEBuNMAw0T95fvqL7kY%3d',
+    '?paginationToken=0%2fckpFbE7Unweup%2blG5RBnhtM9m0HqCXjdWg1Gqi10E%3d',
+    '?paginationToken=12hn47%2bE7G1zIngAaFaotD0mM%2fUdDhRImqrJn69OQFY%3d',
+    '?paginationToken=NVtltkTYwgDoLP8Ofbi9fo3l72ob31ZQgBmcCSUIXdA%3d',
+    '?paginationToken=Vr3Z9h%2bNS22S92DQdYyNFFWHVqaIidOYutlRzVG6Bwg%3d',
+    '?paginationToken=YlmNgWxhfjZiDXBVde9Pi8mN3vktB99RSkiBEJSMz%2bU%3d',
+    '?paginationToken=KWX66gQ35rkneW%2fbwq8w5hCXD2CEcOyE%2bWaSdNQMVn4%3d',
+    '?paginationToken=vNcPbYPD5eCw%2fKkt11rnhSLLi9G5icx89LBfsv6k%2bdE%3d',
+    '?paginationToken=bDSV8neWTO7YNw8Xa%2fbMqIpmw8LRuLESwoxHg6tBndg%3d',
+    '?paginationToken=%2fUJ%2bq86qvIbGzDaNG8iVBKQ1IoWAM3s1EVObq%2fKlRS4%3d',
+    '?paginationToken=i7xCq8v0ADAwoaMDJB39PfwY%2br3YcmEVRV1lmqtRcXU%3d',
+    '?paginationToken=N86%2fykxls0YgiPoMdoyU5GC6Y1%2fRSXvx%2bpBYAtZF5ac%3d',
+    '?paginationToken=C59N8s6rQmVbORAKRnk%2bA0Vi18vPMZpuUdvHMbjgHcU%3d',
+    '?paginationToken=t9Z6rni%2bdwrSD1RzXK2XlUOcWH%2fBFFxdnJmvaTodvvE%3d',
+    '?paginationToken=LhfyNsIIj1tOvXGni2Hwtm5E%2bP%2fBR%2bfL%2bvrN%2fNYdcOw%3d',
+    '?paginationToken=lcGctq6M0WVpBCrmkYLFUBXXGhn9snWP0eKlP3FEdeQ%3d',
+    '?paginationToken=NRGJA9qtkgycubalEJX9E9Rls5Lw8oLbLtxetNsfH68%3d',
+    '?paginationToken=%2fEo5HEwqfK3L83DcD%2b63zQv0bhlfXnmfFqyGd6Up800%3d',
+    '?paginationToken=%2fASIBFWmIUvC7A%2bKS3WRsx0hcaRAQQiOI0VHTDJUwjw%3d',
+    '?paginationToken=VfrdhauCiwOEf2y%2f%2bh1zbuZc52eXVuybw8hxiN3H3T0%3d'
+  ]
+  const getArtists = async () => {
     let artistArray = [];
-    artistArray.push(artistResult.data);
+    for (const token of tokens) {
+      let artistResponse = await fetch (`https://www.wikiart.org/en/api/2/UpdatedArtists${token}`)
+      let artistResult = await artistResponse.json();
+      console.log(artistResult.paginationToken);
+      for (let i=0; i <artistResult.data.length; i++) {
+        artistArray.push(artistResult.data[i]);
+      }
+    }
     res.json(artistArray);
   };
 
   getArtists();
+});
+  // const getArtists = async (token) => {
+  //   let artistResponse = null;
+  //   if (token !== undefined) {
+  //     artistResponse = await fetch(
+  //       `https://www.wikiart.org/en/api/2/UpdatedArtists?paginationToken=${token}`
+  //     );
+  //   } else {
+  //     artistResponse = await fetch(
+  //       `https://www.wikiart.org/en/api/2/UpdatedArtists`
+  //     );
+  //   }
+
+  //   let artistResult = await artistResponse.json();
+
+  //   let artistArray = [];
+  //   artistArray.push(artistResult.data);
+  //   res.json(artistArray);
+  // };
+
+  // getArtists();
 
 
 
@@ -109,7 +152,6 @@ app.get("/artists", (req, res) => {
 
 // fetchRequest(`https://www.wikiart.org/en/api/2/UpdatedArtists`)
 
-});
 
 app.post("/pba", (req, res) => {
   const id = req.body.id;
--- src/utility/getArtists.js
+++ src/utility/getArtists.js
@@ -1,13 +1,13 @@
 const getArtists = async () => {
   
 
-const response = await fetch(
-  `http://localhost:3333/artists`
-);
-const artist = await response.json();
-
-
-return artist[0]
-};
-
-export default getArtists;
+  const response = await fetch(
+    `http://localhost:3333/artists`
+  );
+  const artist = await response.json();
+  console.log('from getArtists: ', artist);
+  
+  return artist
+  };
+  
+  export default getArtists;
